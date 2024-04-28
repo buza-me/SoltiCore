ed396eb07b4a97e62726e0d07717b4f6bf585088
kbuza <buza.work@gmail.com>
2024-04-29 02:04:47 +0300

    Created AddOn with base amount of sound files.

---
+++ SoltiCore.lua
@@ -0,0 +1,40 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------
+-- Female:
+LSM:Register("sound", "Female: Danger", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_danger.mp3")
+LSM:Register("sound", "Female: Drums", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_drums.mp3")
+LSM:Register("sound", "Female: Link", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_link.mp3")
+LSM:Register("sound", "Female: Run Away", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_run_away.mp3")
+LSM:Register("sound", "Female: Run In", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_run_in.mp3")
+LSM:Register("sound", "Female: Run Out", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_run_out.mp3")
+LSM:Register("sound", "Female: Safe", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_safe.mp3")
+LSM:Register("sound", "Female: Stack", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_stack.mp3")
+LSM:Register("sound", "Female: Stay", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_stay.mp3")
+LSM:Register("sound", "Female: Stop", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_stop.mp3")
+LSM:Register("sound", "Female: Clear", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_clear.mp3")
+LSM:Register("sound", "Female: Disabled", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_disabled.mp3")
+LSM:Register("sound", "Female: Stunned", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_stunned.mp3")
+LSM:Register("sound", "Female: Armageddon", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\female_armageddon.mp3")
+----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------
+-- Male:
+LSM:Register("sound", "Male: Danger", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_danger.mp3")
+LSM:Register("sound", "Male: Drums", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_drums.mp3")
+LSM:Register("sound", "Male: Link", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_link.mp3")
+LSM:Register("sound", "Male: Run Away", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_run_away.mp3")
+LSM:Register("sound", "Male: Run In", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_run_in.mp3")
+LSM:Register("sound", "Male: Run Out", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_run_out.mp3")
+LSM:Register("sound", "Male: Safe", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_safe.mp3")
+LSM:Register("sound", "Male: Stack", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_stack.mp3")
+LSM:Register("sound", "Male: Stay", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_stay.mp3")
+LSM:Register("sound", "Male: Stop", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_stop.mp3")
+LSM:Register("sound", "Male: Clear", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_clear.mp3")
+LSM:Register("sound", "Male: Disabled", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_disabled.mp3")
+LSM:Register("sound", "Male: Stunned", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_stunned.mp3")
+LSM:Register("sound", "Male: Armageddon", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\male_armageddon.mp3")
+----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------
+-- Misc:
+LSM:Register("sound", "R2D2 scream", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\r2d2_scream.mp3")
+LSM:Register("sound", "Plater Wood", "Interface\\AddOns\\SoltiCore\\Media\\Sounds\\PlaterWood.ogg")
